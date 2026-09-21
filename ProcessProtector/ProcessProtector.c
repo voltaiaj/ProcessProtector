a704212573b6931b0370476973487a00ee648666
voltaiaj <Alexander.Voltaire@gmail.com>
2026-09-20 20:48:14 -0500

    adding object registration initial

--- ProcessProtector/ProcessProtector.c
+++ ProcessProtector/ProcessProtector.c
@@ -6,17 +6,43 @@
 VOID DriverUnload(PDRIVER_OBJECT);
 NTSTATUS ProcessProtectorCreateClose(PDEVICE_OBJECT, PIRP);
 NTSTATUS ProcessProtectorIoControl(PDEVICE_OBJECT, PIRP);
+OB_PREOP_CALLBACK_STATUS OnPreOpenProcess(PVOID, POB_PRE_OPERATION_INFORMATION);
+
+FAST_MUTEX g_ProtectedProcessesMutex;
+
+typedef struct _Globals{
+	LIST_ENTRY ProtectedProcessesListHead;
+} Globals;
+
+Globals g_Globals;
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
 	UNREFERENCED_PARAMETER(RegistryPath);
 	KdPrint((DRIVER_PREFIX "DriverEntry called\n"));
-	// Set up the driver unload routine
+	// Set up the driver unload routine	
 	DriverObject->DriverUnload = DriverUnload;
 	DriverObject->MajorFunction[IRP_MJ_CREATE] =
 		DriverObject->MajorFunction[IRP_MJ_CLOSE] = ProcessProtectorCreateClose;
 	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = ProcessProtectorIoControl;
 	// Additional initialization code can go here
+	OB_OPERATION_REGISTRATION obRegistration = {
+		PsProcessType,
+		OB_OPERATION_HANDLE_CREATE | OB_OPERATION_HANDLE_DUPLICATE,
+		OnPreOpenProcess,
+		NULL
+	};
+	OB_CALLBACK_REGISTRATION obCallbackRegistration = {
+		OB_FLT_REGISTRATION_VERSION,
+		1
+	};
+
+	obCallbackRegistration.OperationRegistration = &obRegistration;
+	RtlInitUnicodeString(&obCallbackRegistration.Altitude, L"12345.6171");
+
+	PVOID regHandle;
+	ObRegisterCallbacks(&obCallbackRegistration, &regHandle); // Placeholder for actual callback registration
+
 	return STATUS_SUCCESS;
 }
 
@@ -64,4 +90,22 @@ NTSTATUS ProcessProtectorIoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 	Irp->IoStatus.Information = 0;
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
 	return STATUS_SUCCESS;
+}
+
+OB_PREOP_CALLBACK_STATUS OnPreOpenProcess(PVOID RegistrationContext, POB_PRE_OPERATION_INFORMATION OperationInformation)
+{
+	UNREFERENCED_PARAMETER(RegistrationContext);
+	UNREFERENCED_PARAMETER(OperationInformation);
+	KdPrint((DRIVER_PREFIX "OnPreOpenProcess called\n"));
+	// Here you would implement the logic to check if the process is protected
+	// and deny access if necessary. For now, we just log the event.
+	if (OperationInformation->ObjectType == PsProcessType)
+	{
+		PEPROCESS targetProcess = (PEPROCESS)OperationInformation->Object;
+		HANDLE targetPid = PsGetProcessId(targetProcess);
+		KdPrint((DRIVER_PREFIX "Attempt to open process with PID: %u\n", (ULONG)(ULONG_PTR)targetPid));
+		// Implement your protection logic here
+
+	}
+	return OB_PREOP_SUCCESS; // Returning OB_PREOP_SUCCESS means we are not denying access
 }
